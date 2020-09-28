c8255ef454082e90d755b8500d590fbc00a1b472
Paulo Reis <paulork07@gmail.com>
2020-09-28 13:39:52 -0300

    add: user agent

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ puppeteer.launch({
   executablePath: "/opt/google/chrome-unstable/google-chrome",
   args: [
     "--no-sandbox",
+    '--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36',
   ]
 }).then(async browser => {
 
